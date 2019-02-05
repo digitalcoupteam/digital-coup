20745ec432c70ffc097566878e46c406eaa7092a
Hero's Dad <cody.monta@gmail.com>
2019-02-04 19:21:24 -0500

    working on auth

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,29 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/newUsers", function(req, res) {
+    db.User.findAll().then(function(result) {
+      res.json(result);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/newUser", function(req, res) {
+    const newUser = req.body;
+    db.User.create({
+      userName: newUser.userName,
+      password: newUser.password,
+      email: newUser.email
+    }).then(function(result) {
+      res.json(result);
+      
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
 };
