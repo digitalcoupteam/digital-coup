73c7ae2e7dabb3b93b846bafb10eab7d8b18640b
Hero's Dad <cody.monta@gmail.com>
2019-02-06 10:23:43 -0600

    committing before switching to master

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,48 +1,44 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/newUsers", function(req, res) {
-    db.User.findAll().then(function(result) {
+  app.get("/api/newUsers", function (req, res) {
+    db.User.findAll().then(function (result) {
       res.json(result);
     });
   });
 
   // Create a new example
-  app.post("/api/newUser", function(req, res) {
+  app.post("/api/newUser", function (req, res) {
     const newUser = req.body;
     db.User.create({
       email: newUser.email,
       password: newUser.password,
       userName: newUser.userName
-    }).then(function(result) {
+    }).then(function (result) {
       res.json(result);
-      
+
     });
   });
 
 
-  app.post('/api/login', function(req, res) {
+  app.post('/login', function (req, res) {
     const attemptedLogin = req.body;
     console.log(attemptedLogin.loginUsername)
     db.User.findOne({
       where: {
-          userName: attemptedLogin.loginUsername,
-          password: attemptedLogin.loginPassword
+        userName: attemptedLogin.loginUsername,
+        password: attemptedLogin.loginPassword
       }
-  }).then(function (result) {
-      if (result) {
-        console.log('user exists')
-        res.json(result);  
-      } else {
-        console.log('no user exists')
-      }
-  });
-})
+    }).then(function (result) {
+      res.json(result);
+
+    });
+  })
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
